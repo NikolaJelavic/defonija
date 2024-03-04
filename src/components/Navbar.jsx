e648a6e63c14092a878446516f273b78fb1fff10
NikolaJelavic <jelavicnikola@gmail.com>
2024-03-04 16:42:35 +0100

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,88 @@
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+
+const NavLink = ({ to, children, onClick }) => (
+  <li>
+    <Link
+      to={to}
+      className="text-2xl bg-slate-100 px-2  rounded-xl my-3 font-light text-slate-500 hover:text-slate-400"
+      onClick={onClick}
+      style={{
+        fontFamily: "'Cormorant Unicase', sans-serif",
+      }}
+    >
+      {children}
+    </Link>
+  </li>
+);
+
 
 
 export default function Navbar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <nav className="w-full bg-slate-100 flex py-5 px-1 h-20">
-      <ul className="flex justify-between md:justify-around w-full">
-        <Link
+    // <nav className="w-full bg-slate-100 flex py-5 px-1 h-20">
+    <nav className=" p-6 bg-slate-100">
+
+        <div className="flex justify-between mx-auto relative z-10">
+
+        <button
+          className="lg:hidden text-white text-3xl focus:outline-none bg-slate-300 p-4 justify-left"
+          onClick={toggleMenu}
+        >
+          &#9776;
+        </button>
+
+
+      {/* <ul className="flex justify-between md:justify-around w-full"> */}
+      <ul
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } lg:flex lg:justify-evenly  lg:space-x-4 lg:w-full w-40 absolute lg:relative bg-slate-300 lg:bg-transparent top-full lg:top-0 left-0 lg:left-auto  lg:mt-0 p-4 lg:p-0 `}
+        >
+        <NavLink
           to="/"
-          className="text-slate-500 font-black hover:text-slate-400 text-lg"
+          onClick={toggleMenu}
         >
           O nama
-        </Link>
+        </NavLink>
 
-        <Link
+        <NavLink
           to="/program"
-          className="text-slate-500 font-black hover:text-slate-400 text-lg"
+          onClick={toggleMenu}
         >
           Program
-        </Link>
+        </NavLink>
 
-        <Link
+        <NavLink
           to="/linkovi"
-          className="text-slate-500 font-black hover:text-slate-400 text-lg"
+          onClick={toggleMenu}
         >
           Linkovi
-        </Link>
+        </NavLink>
 
-        <Link
+        <NavLink
           to="/Arhiva"
-          className="text-slate-500 font-black hover:text-slate-400 text-lg"
+          onClick={toggleMenu}
         >
           Arhiva
-        </Link>
+        </NavLink>
 
-        <Link
+        <NavLink
           to="/kontakt"
-          className="text-slate-500 font-black hover:text-slate-400 text-lg"
+          onClick={toggleMenu}
         >
           Kontakt
-        </Link>
+        </NavLink>
       </ul>
+      </div>
     </nav>
   );
 }
