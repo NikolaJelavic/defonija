e70cdf15ce3c038d1b2fff90d5169585099bdc17
NikolaJelavic <jelavicnikola@gmail.com>
2024-03-05 02:09:02 +0100

    first event added.

--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -5,8 +5,64 @@ export default function Program() {
     
 
     return (
-        <>
-            <p className="my-80 text-center">Program</p>
+        <>  
+            <main class="flex justify-center mb-32">
+                <article className="bg-slate-200 w-11/12 md:w-1/2 mt-6 rounded-xl p-5 min-h-screen " >
+                    <h2 className="text-center text-3xl">Defonija: MIMAN (NO)</h2>
+                    <p className="text-center"> 🚩 Klub Gromka, AKC Metelkova mesto</p>
+                    <br />
+                    <img src="https://res.cloudinary.com/dapwniqqn/image/upload/v1709599601/Defonija/Program/MIMAN_qrh36s.png" alt="" />
+                    <br />
+                    <p>
+                    Udarna, a tudi rahlo zastrta in skrivnostna elektroakustična godba, ki se napaja v free in spiritualnem jazzu, koketira s klasično muziko in noizom, prežemajo pa jo elementi ljudske glasbe azijske, afriške, angleške in nordijske provenience.
+                    </p>
+                    <br />
+                    <p>
+                    📌 MIMAN (NO)
+                    </p>
+                    <ul>
+                        <li>Hans P. Kjorstad — gosli</li>
+                        <li>Andreas Røysum — klarinet, basovski klarinet, flavta</li>
+                        <li>Egil Kalman — kontrabas, modularni sintetizator</li>
+                    </ul>
+                    <br />
+                    <p>
+                    S koncertom začnemo dovolj točno ob 21. uri; blagajna, vrata in šank se odpro ob 20.30.
+                    </p>
+                    <br />
+                    <p>
+                    Pred nami je ena izmed najaktivnejših (in najatraktivnejših) skandinavskih zasedb mlajše generacije, zbrane v kulturni formaciji Motvind, kar pomeni: z vetrom v ledja. Trio Miman zna iz improviziranega preigravanja ustvariti vseobsegajoče, mojstrsko izkristalizirane fragmente glasbenih slogov in gradivo potem predstavi skrivnostno; nerazložljivo; kakor v sanjah. Miman izrazno sicer stremijo k uglašenemu idealu mnoštva svetovnih godb, a se obenem ne otepajo niti groovy momentov, pri katerih občinstvo vrti glavo, niti strogega, počasneje tekočega glasbenega podjetja. Vsi trije muzičisti so obenem tudi člani izjemnega Marthe Lea Banda in zasedbe Völvur (ta je leta 2021 z Alasdairjem Robertsom izdal neverjetno lep album The Old Fabled), dvojico najdemo v Andreas Røysum Ensemblu, veliko pa igrajo tudi v raznoraznih folk (npr. Vassvik) in noiz zasedbah. Tako ima goslač Kjorstad, vzemimo, krasen duo s svojim bratom Rasmusom, v paru rad igra tudi s čelistom Ernstom Reijsegerjem, najdemo ga tudi v veliki jazzovski zasedbi Frode Haltli Avant Folk ...
+                    </p>
+                    <br />
+                    <p>
+                    Miman se odlično znajde tudi v sodelovanju z bendi, najsi gre za folk ali noiz. Njihov zvok se odlikuje s svojevrstno ostrino, ki se oslanja na indijske, norveške in angleške glasbene tradicije, free in spiritualni jazz ter klasično glasbo. Kakor je pri mladi sceni, zbrani v kulturni formaciji Motvind, že v navadi, zelo težko napovemo, kaj konkretno nam je prejeti na pričujočem koncertu. Morda improvizirane kose, ki zvenijo kot premišljene in komponirane pesmi? Nemara mehko združena popotovanja po svetovih folka, skoz katera subtilno prenicajo in se prepletajo afriški jazz, azijski folk in namišljeni nordijski songi? Na zadnjem albumu trojka pravi, da je muzika blizu poslušalstvu, ki ga pritegnejo Terry Riley, Don Cherry, Anthony Braxton, tradicijska goslaška godba in Art Ensemble of Chicago ... Vsekakor se pustimo presenetiti.
+                    </p>
+                    <br />
+                    <p>
+                    Miman so imeli pred kratkim turnejo, na kateri so izvajali dela srednjeveške skladateljice, glasbenice, zdravilke in mistikinje Hildegard von Bingen (1098-1179), sicer pa v svojo sredo zelo radi privabijo kar najrazličnejše goste in gostje; z njimi so igrali, denimo, Ernst Reijseger, Tobias Delius, Axel Dörner, Frode Haltli, Håkon Thelin, Guro Moe, Jasper Stadhouders, Frode Gjerstad, Christian Wallumrød in Rhys Chatham. Pri nas so bili Miman že parkrat; tako so nazadnje v Bistrici ob Sotli marca lani nastopili s — tačas pokojnim — Tristanom Honsingerjem, bratoma Drašler, Markom Jeničem in Andrejem Boštjančičem.
+                    </p>
+                    
+                    <a href="http://mimantrio.com/" target="_blank" rel="noopener noreferrer" className="text-cyan-700">http://mimantrio.com/</a>
+
+                    <br /> <br />
+                    <p>
+                    Zadnji album Miman je naslovljen 1000 Bitar, posnet je bil med tridnevno (in trinočno) seanso v bližini slikovitega fjorda Leikanger in Sogn, izšel je leta 2021, mastering je izvedel Giuseppe Ielasi, njega naslovnico pa se dali Miman v oblikovanje Lojsi af Geijerstam, ki je opremila precej albumov švedske progresivne muzike iz sedemdesetih let:
+                    </p>
+                    <a href="https://miman.bandcamp.com/album/1000-bitar" target="_blank" rel="noopener noreferrer" className="text-cyan-700">https://miman.bandcamp.com/album/1000-bitar</a>
+
+                    <br /> <br />
+                    <p>
+                    Miman med špilom (klub Blow Out, Oslo, Norveška, oktobra 2022):
+                    </p>
+                    <a href="https://www.youtube.com/watch?v=k1XETzNEcUE&t=2s" target="_blank" rel="noopener noreferrer" className="text-cyan-700">https://www.youtube.com/watch?v=k1XETzNEcUE&t=2s</a>
+
+                    <br /> <br /> <br />
+                    <p>
+                    Koncertni cikel Defonija sofinancirata Ministrstvo za kulturo Republike Slovenije in Mestna občina Ljubljana — oddelek za kulturo.
+                    </p>
+
+                </article>
+            </main>
             <Footer/>
         </>
     )
