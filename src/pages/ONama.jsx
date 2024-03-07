396e56f9da5817471cc3510e45a305a4135f6b68
NikolaJelavic <jelavicnikola@gmail.com>
2024-03-07 21:54:36 +0100

    Program

--- src/pages/ONama.jsx
+++ src/pages/ONama.jsx
@@ -5,7 +5,14 @@ export default function ONama() {
 
     return (
         <>
-            <p className="my-80 text-center">O nama</p>
+             <main class="flex justify-center mb-32">
+                <article className="bg-slate-200 w-11/12 md:w-1/2 mt-6 rounded-xl p-5 min-h-full " >
+                    <img src="https://res.cloudinary.com/dapwniqqn/image/upload/v1709844144/Defonija/AboutUs/about_geg8fr.jpg" alt="ONama" />
+                    <p className="text-xl p-4">
+                    For over 20 years an extremely important avenue for explorative music in Slovenia. Held at Club Gromka in the Autonomous Cultural Centre Metelkova mesto, Defonija runs a free-wheeling programme that can easily pair experimental rock with improv gigs in the same evening. Though, it does have a penchant for the great tradition of no-nonsense free jazz. 
+                    </p>
+                </article>
+             </main>      
             <Footer/>
         </>
     )
