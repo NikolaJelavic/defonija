3ac743beadd13996f9c1cbaaa9c992c21cf97b65
NikolaJelavic <jelavicnikola@gmail.com>
2024-03-11 19:04:35 +0100

    many things

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,5 @@
-
 import { Link } from "react-router-dom";
 import { useState } from "react";
-
 
 const NavLink = ({ to, children, onClick }) => (
   <li>
@@ -18,8 +16,6 @@ const NavLink = ({ to, children, onClick }) => (
   </li>
 );
 
-
-
 export default function Navbar() {
   const [isMenuOpen, setMenuOpen] = useState(false);
 
@@ -28,60 +24,42 @@ export default function Navbar() {
   };
 
   return (
-    // <nav className="w-full bg-slate-100 flex py-5 px-1 h-20">
-    <nav className=" p-6 bg-slate-100">
-
-        <div className="flex justify-between mx-auto relative z-10">
-
-        <button
-          className="lg:hidden text-white text-3xl focus:outline-none bg-slate-300 p-4 justify-left"
-          onClick={toggleMenu}
-        >
-          &#9776;
-        </button>
-
-
-      {/* <ul className="flex justify-between md:justify-around w-full"> */}
-      <ul
+    <nav className="p-2 md:p-6 bg-slate-100">
+      <div className="flex justify-between items-center mx-auto relative z-10">
+        <div className="flex items-center">
+          <button
+            className="lg:hidden text-white text-3xl focus:outline-none bg-slate-300 p-4 justify-left"
+            onClick={toggleMenu}
+          >
+            &#9776;
+          </button>
+          <img
+            src="https://res.cloudinary.com/dapwniqqn/image/upload/v1710179436/Defonija/Logo/DEFONIJA_LOGO_KOLOR_FLAT_asxxmz.png"
+            alt="Defonija Logo"
+            className="h-16 w-screen flex justify-center items-center lg:hidden"
+          />
+        </div>
+        <ul
           className={`${
             isMenuOpen ? "block" : "hidden"
           } lg:flex lg:justify-evenly  lg:space-x-4 lg:w-full w-40 absolute lg:relative bg-slate-300 lg:bg-transparent top-full lg:top-0 left-0 lg:left-auto  lg:mt-0 p-4 lg:p-0 `}
         >
-        <NavLink
-          to="/"
-          onClick={toggleMenu}
-        >
-          O nama
-        </NavLink>
-
-        <NavLink
-          to="/program"
-          onClick={toggleMenu}
-        >
-          Program
-        </NavLink>
-
-        <NavLink
-          to="/povezave"
-          onClick={toggleMenu}
-        >
-          Povezave
-        </NavLink>
-
-        <NavLink
-          to="/arhiv"
-          onClick={toggleMenu}
-        >
-          Arhiva
-        </NavLink>
-
-        <NavLink
-          to="/kontakt"
-          onClick={toggleMenu}
-        >
-          Kontakt
-        </NavLink>
-      </ul>
+          <NavLink to="/" onClick={toggleMenu}>
+            O nas
+          </NavLink>
+          <NavLink to="/program" onClick={toggleMenu}>
+            Program
+          </NavLink>
+          <NavLink to="/povezave" onClick={toggleMenu}>
+            Povezave
+          </NavLink>
+          <NavLink to="/arhiv" onClick={toggleMenu}>
+            Arhiv
+          </NavLink>
+          <NavLink to="/kontakt" onClick={toggleMenu}>
+            Kontakt
+          </NavLink>
+        </ul>
       </div>
     </nav>
   );
