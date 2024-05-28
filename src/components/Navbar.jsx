7fefcb83b87c5f8f931d92560b24fa4dc743d5f8
NikolaJelavic <jelavicnikola@gmail.com>
2024-05-28 18:04:02 +0200

    new menu images added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,15 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
-const NavLink = ({ to, children, onClick }) => (
-  <li>
-    <Link
-      to={to}
-      className="text-2xl bg-slate-100 px-2  rounded-xl my-3 font-light text-slate-500 hover:text-slate-400"
-      onClick={onClick}
-      style={{
-        fontFamily: "'Cormorant Unicase', sans-serif",
-      }}
-    >
-      {children}
-    </Link>
-  </li>
-);
-
-export default function Navbar() {
-  const [isMenuOpen, setMenuOpen] = useState(false);
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <nav className="p-2 md:p-6 bg-slate-100">
+    <nav className="p-2 md:p-6">
       <div className="flex justify-between items-center mx-auto relative z-10">
         <div className="flex items-center">
           <button
@@ -42,25 +27,46 @@ export default function Navbar() {
         <ul
           className={`${
             isMenuOpen ? "block" : "hidden"
-          } lg:flex lg:justify-evenly  lg:space-x-4 lg:w-full w-40 absolute lg:relative bg-slate-300 lg:bg-transparent top-full lg:top-0 left-0 lg:left-auto  lg:mt-0 p-4 lg:p-0 `}
+          } lg:flex lg:justify-evenly lg:space-x-4 lg:w-full w-60 absolute lg:relative bg-slate-300 lg:bg-transparent top-full lg:top-0 left-0 lg:left-auto lg:mt-0 p-2 lg:p-0`}
         >
-          <NavLink to="/" onClick={toggleMenu}>
-            O nas
+          <NavLink to="/" onClick={toggleMenu} className="flex items-center">
+            <img
+              src="https://res.cloudinary.com/dapwniqqn/image/upload/v1716910762/Defonija/Menu/About_lqcxlw.jpg"
+              alt="About Us"
+              className="h-20  lg:h-16 lg:w-72"
+            />
           </NavLink>
-          <NavLink to="/program" onClick={toggleMenu}>
-            Program
+          <NavLink to="/program" onClick={toggleMenu} className="flex items-center">
+            <img
+              src="https://res.cloudinary.com/dapwniqqn/image/upload/v1716910763/Defonija/Menu/Events_t65c7b.jpg"
+              alt="Program"
+              className="h-20  lg:h-16 lg:w-72"
+            />
           </NavLink>
           <NavLink to="/povezave" onClick={toggleMenu}>
+        
             Povezave
           </NavLink>
           <NavLink to="/arhiv" onClick={toggleMenu}>
-            Arhiv
+          <img
+              src="https://res.cloudinary.com/dapwniqqn/image/upload/v1716910763/Defonija/Menu/Archive_ucwcji.jpg"
+              alt="Program"
+              className="h-20  lg:h-16 lg:w-72"
+            />
+           
           </NavLink>
           <NavLink to="/kontakt" onClick={toggleMenu}>
-            Kontakt
+          <img
+              src="https://res.cloudinary.com/dapwniqqn/image/upload/v1716910762/Defonija/Menu/Contact_uxnjhe.jpg"
+              alt="Program"
+              className="h-20  lg:h-16 lg:w-72"
+            />
+            
           </NavLink>
         </ul>
       </div>
     </nav>
   );
-}
+};
+
+export default Navbar;
